0510b1a8d52780ee08e7d2fa3e5c7d267d515d0a
Zachary Moroney <s3784630@student.rmit.edu.au>
2020-09-21 15:07:31 +1000

    Test

---
+++ TestScript.sh
@@ -0,0 +1,18 @@
+#/bin/#!/usr/bin/env bash
+#Basic while loop
+
+counter=1
+while [ $counter -le 10 ]
+do
+  echo $counter
+  ((counter++))
+done
+echo All done
+
+#Basic for loop
+names='Zachary Moroney'
+for name in $names
+do
+  echo $name
+done
+echo All done
